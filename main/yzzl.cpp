9429651f9ddfc92b13f69f336b05774a00ee4d76
Kewth <Kewth.K.D@outlook.com>
2019-01-30 20:02:47 +0800

    将 Player 加入行动队列统一行动

--- main/yzzl.cpp
+++ main/yzzl.cpp
@@ -4,6 +4,7 @@
 #include "../include/users.h"
 #include "../include/input.h"
 #include "../include/cursor.h"
+#include "../include/othertodo.h"
 
 int main(int, const char **) {
 	cursor::hide();
@@ -13,8 +14,10 @@ int main(int, const char **) {
 		return 1;
 	map::Map *city = new map::Maincity();
 	self->join_map(city);
+	othertodo::Queue que;
+	que.add_people(self);
 	while(self->get_map()) {
-		self->todo();
+		que.todo();
 	}
 	cursor::display();
 	remove("py_output");
